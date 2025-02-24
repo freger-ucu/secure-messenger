8ac10316041960d6c1238b643695ae2ecbd79379
Mykhailo Rykhalskyi <rykhalskyi.pn@ucu.edu.ua>
2025-02-24 18:10:33 +0200

    Initial db setup

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "rest_framework",
+    "api"
 ]
 
 
@@ -87,8 +88,12 @@ WSGI_APPLICATION = "backend.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'messenger',
+        'USER': 'messenger',
+        'PASSWORD': 'development',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
