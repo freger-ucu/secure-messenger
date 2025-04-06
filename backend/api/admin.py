96c4ae7652ad81b36e82e981ae225759934b646a
DuloVod <vladv@MacBook-Air-Vlad.local>
2025-04-06 12:54:38 +0300

    chat model

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from api.models import ChatMessage
 
-# Register your models here.
+class ChatMessageAdmin(admin.ModelAdmin):
+    list_editable = ['is_read', 'message']
+    list_display = ['user','sender', 'reciever', 'is_read', 'message']
+
+admin.site.register( ChatMessage,ChatMessageAdmin)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-"kdsnjhx"
+class ChatMessage(models.Model):
+    """ chat message class """
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+    reciever = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reciever')
+
+    message = models.CharField(max_length=1000)
+    is_read = models.BooleanField(default=False)
+    date = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['date']
+        verbose_name_plural = "Message"
+
+    def __str__(self):
+        return f'{self.sender} - {self.reciever}'
+
+    @property
+    def sender_profile(self):
+        sender_profile = User.objects.get(user=self.sender)
+        return sender_profile
+
+    @property
+    def reciever_profile(self):
+        reciever_profile = User.objects.get(user=self.reciever)
+        return reciever_profile
