736c723a462b12a2e6ff14a3779f374c0cb87b36
DuloVod <vladv@MacBook-Air-Vlad.local>
2025-04-06 13:24:24 +0300

    chatgroup and groupmessege classes

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,8 +1,12 @@
 from django.contrib import admin
-from api.models import ChatMessage
+from .models import ChatGroup, GroupMessage
+# from api.models import ChatMessage
 
-class ChatMessageAdmin(admin.ModelAdmin):
-    list_editable = ['is_read', 'message']
-    list_display = ['user','sender', 'reciever', 'is_read', 'message']
+# class ChatMessageAdmin(admin.ModelAdmin):
+#     list_editable = ['is_read', 'message']
+#     list_display = ['user','sender', 'reciever', 'is_read', 'message']
 
-admin.site.register( ChatMessage,ChatMessageAdmin)
+# admin.site.register( ChatMessage,ChatMessageAdmin)
+
+admin.site.register(ChatGroup)
+admin.site.register(GroupMessage)
--- backend/api/models.py
+++ backend/api/models.py
@@ -1,29 +1,49 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-class ChatMessage(models.Model):
-    """ chat message class """
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
-    sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
-    reciever = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reciever')
+# class ChatMessage(models.Model):
+#     """ chat message class """
+#     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user')
+#     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name='sender')
+#     reciever = models.ForeignKey(User, on_delete=models.CASCADE, related_name='reciever')
 
-    message = models.CharField(max_length=1000)
-    is_read = models.BooleanField(default=False)
-    date = models.DateTimeField(auto_now_add=True)
+#     message = models.CharField(max_length=1000)
+#     is_read = models.BooleanField(default=False)
+#     date = models.DateTimeField(auto_now_add=True)
 
-    class Meta:
-        ordering = ['date']
-        verbose_name_plural = "Message"
+#     class Meta:
+#         ordering = ['date']
+#         verbose_name_plural = "Message"
+
+#     def __str__(self):
+#         return f'{self.sender} - {self.reciever}'
+
+#     @property
+#     def sender_profile(self):
+#         sender_profile = User.objects.get(user=self.sender)
+#         return sender_profile
+
+#     @property
+#     def reciever_profile(self):
+#         reciever_profile = User.objects.get(user=self.reciever)
+#         return reciever_profile
+
+
+class ChatGroup(models.Model):
+    group_name = models.CharField(max_length=128, unique=True)
 
     def __str__(self):
-        return f'{self.sender} - {self.reciever}'
+        return str(self.group_name)
 
-    @property
-    def sender_profile(self):
-        sender_profile = User.objects.get(user=self.sender)
-        return sender_profile
 
-    @property
-    def reciever_profile(self):
-        reciever_profile = User.objects.get(user=self.reciever)
-        return reciever_profile
+class GroupMessage(models.Model):
+    group = models.ForeignKey(ChatGroup, related_name='chat_messages', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    body = models.CharField(max_length=300)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f'{self.author.username} : {self.body}'
+
+    class Meta:
+        ordering = ['-created']
