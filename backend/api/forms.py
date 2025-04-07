628e0b028d561840166ed72598ef62cf165c6272
DuloVod <vladv@MacBook-Air-Vlad.local>
2025-04-07 14:51:07 +0300

    forms and view

---
+++ backend/api/forms.py
@@ -0,0 +1,11 @@
+from django.forms import ModelForm
+from django import forms
+from .models import *
+
+class ChatmessegeCreateForm(ModelForm):
+    class Meta:
+        model = GroupMessage
+        fields = ['body']
+        widgets = {
+            'body' : forms.TextInput(attrs={'placeholder': 'Add messege', 'class': 'p-4 text-black', 'maxlength': 300, 'autofocus': True})
+        }
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.contrib.auth.decorators import login_required
+from .models import *
+from .forms import ChatmessegeCreateForm
 
-# Create your views here.
+@login_required
+def chat_view(request):
+    chat_group = get_object_or_404(Chat, group_name='public_chat')
+    chat_messages = chat_group.chat_messege.all()[:30]
+    form = ChatmessegeCreateForm()
+
+    if request.method == 'POST':
+        form = form = ChatmessegeCreateForm(request.POST)
+        if form.is_valid:
+            message = form.save(commit=False)
+            message.author = request.user
+            message.group = chat_group
+            message.save()
+            return redirect('home')
+
+    return render(request, 'a_rtchat/chathtml', {chat_messages : 'chat_messeges', form: 'form'})
